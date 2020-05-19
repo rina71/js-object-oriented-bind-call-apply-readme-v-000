88f24f70f98ef05ea7b79e3ce776e0994464900e
Yurina Shin <yurinas@gmail.com>
2020-05-19 03:22:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ class Event {
         this.keywords = keywords;
     }
 }
- 
+
 class User {
     constructor(name, interests) {
         this.name = name;
         this.interests = interests;
     }
- 
+
     matchInterests(event) {
         return event.keywords.some(
             function(word) {
@@ -20,8 +20,8 @@ class User {
         );
     }
 }
- 
+
 let billy = new User('billy', ['music', 'art', 'movies']);
 let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
- 
+
 billy.matchInterests(freeMusic);
